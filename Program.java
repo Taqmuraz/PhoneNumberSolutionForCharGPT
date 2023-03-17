a20ca230ba3ae3c3f36785d162804b32710a1775
taimuraz.fardzinov <taimuraz.fardzinov@aspose.com>
2023-03-17 18:44:03 +0100

    person example

--- Program.java
+++ Program.java
@@ -1,159 +1,58 @@
+import java.io.PrintStream;
 import java.util.Arrays;
-import java.util.Scanner;
-import java.util.stream.Stream;
 
-interface ItemPrinter
+interface Person
 {
-	void print(String text);
+	void print(PrintStream stream);
+	String getName();
 }
 
-interface Printer
-{
-	ItemPrinter item();
-}
-
-interface MenuItem
-{
-	boolean isName(String name);
-	void print(Printer printer);
-}
-
-class FoodItem implements MenuItem
+class MortalPerson implements Person
 {
 	private String name;
-	private float weight;
-	private int calories;
-	private int price;
-
-	public FoodItem(String name, float weight, int calories, int price)
+	private int age;
+	
+	public MortalPerson(String name, int age)
 	{
 		this.name = name;
-		this.weight = weight;
-		this.calories = calories;
-		this.price = price;
+		this.age = age;
+	}
+
+	public void print(PrintStream stream)
+	{
+		stream.print("Name : ");
+		stream.print(name);
+		stream.print("Age : ");
+		stream.print(age);
 	}
 
 	@Override
-	public boolean isName(String name)
+	public String getName()
 	{
-		return name.equals(this.name);
-	}
-
-	@Override
-	public void print(Printer printer)
-	{
-		ItemPrinter item = printer.item();
-		item.print("Name : " + name);
-		item.print("Weight : " + weight);
-		item.print("Calories : " + calories);
-		item.print("Price : " + price);
+		return name;
 	}
 }
 
-class DrinkItem implements MenuItem
+class GodPerson implements Person
 {
 	private String name;
-	private float volume;
-	private int calories;
-	private int price;
 
-	public DrinkItem(String name, float volume, int calories, int price)
-	{
+	public GodPerson(String name) {
 		this.name = name;
-		this.volume = volume;
-		this.calories = calories;
-		this.price = price;
 	}
 
 	@Override
-	public boolean isName(String name)
+	public String getName()
 	{
-		return name.equals(this.name);
+		return name;
 	}
 
 	@Override
-	public void print(Printer printer)
+	public void print(PrintStream stream)
 	{
-		ItemPrinter item = printer.item();
-		item.print("Name : " + name);
-		item.print("Volume : " + volume);
-		item.print("Calories : " + calories);
-		item.print("Price : " + price);
-	}
-}
-
-class MusicItem implements MenuItem
-{
-	public MusicItem(String name, String author, float length)
-	{
-		this.name = name;
-		this.author = author;
-		this.length = length;
-	}
-
-	private String name;
-	private String author;
-	private float length;
-
-	@Override
-	public boolean isName(String name)
-	{
-		return name.equals(this.name);
-	}
-
-	@Override
-	public void print(Printer printer)
-	{
-		ItemPrinter item = printer.item();
-		item.print("Name : " + name);
-		item.print("Author : " + author);
-		item.print("Length : " + length + " min");
-	}
-}
-
-class SpecialMenu implements MenuItem
-{
-	private String name;
-	private MenuItem[] items;
-
-	public SpecialMenu(String name, MenuItem[] items)
-	{
-		this.name = name;
-		this.items = items;
-	}
-	@Override
-	public boolean isName(String name)
-	{
-		return name.equals(this.name);
-	}
-	@Override
-	public void print(Printer printer)
-	{
-		for(MenuItem item : items) item.print(printer);
-	}
-}
-
-class SeparatedMenu
-{
-	private MenuItem[] items;
-
-	public SeparatedMenu(MenuItem[] adults, MenuItem[] children)
-	{
-		items = Stream.concat(
-			Stream.concat(Arrays.stream(adults), Arrays.stream(children)),
-			Stream.<MenuItem>of(new SpecialMenu("child", children), new SpecialMenu("adult", adults))).toArray(MenuItem[]::new);
-	}
-
-	public void printItem(String name, Printer printer)
-	{
-		for(MenuItem item : items)
-		{
-			if(item.isName(name))
-			{
-				item.print(printer);
-				break;
-			}
-		}
+		stream.print("Name : ");
+		stream.print(name);
+		stream.print(", he is God");
 	}
 }
 
@@ -161,28 +60,17 @@ public class Program
 {
 	public static void main(String[] args) throws Exception
 	{
-		String input;
-		Scanner scanner = new Scanner(System.in);
-		input = scanner.nextLine();
-		scanner.close();
-
-		new SeparatedMenu(
-			new MenuItem[]
-			{
-				new FoodItem("Chicken", 300, 2500, 50),
-				new DrinkItem("Tea", 150, 200, 10),
-				new MusicItem("House on fire", "Sia", 1.5f),
-			},
-			new MenuItem[]
-			{
-				new FoodItem("Small chicken", 150, 1000, 30),
-				new DrinkItem("Milk", 100, 250, 5),
-				new MusicItem("Goodbye moonmen", "Unknown author", 2.5f),
-			}
-		).printItem(input, () ->
+		Person[] persons =
 		{
+			new MortalPerson("Alice", 30),
+			new GodPerson("Jesus"),
+			new GodPerson("Allah"),
+		};
+
+		Arrays.stream(persons).sorted((a, b) -> a.getName().compareTo(b.getName())).forEach(person ->
+		{
+			person.print(System.out);
 			System.out.println();
-			return System.out::println;
 		});
 	}
 }
